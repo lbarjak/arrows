acefe364301c203976a61a93082b39ad0f3e3ad3
lbarjak <barjak.laszlo@gmail.com>
2019-02-15 01:17:56 +0100

    v0.1

--- arrows.js
+++ arrows.js
@@ -1,11 +1,16 @@
 //http://www.winandwinblack.com/pdf/arrows-selection-guide.pdf
-var lbs, length, point, spine
-lbs = 27
-length = 28
-point = 80
-lbs = lbs + (point - 100) * 0.15
+function arrows() {
+    var lbs = 0, length = 0, point = 0, spine = 0
+    
+    lbs = Number(document.getElementById("lbs").value)
+    length = Number(document.getElementById("inches").value)
+    point = Number(document.getElementById("point").value)
+    
+    lbs = lbs + (point - 100) * 0.15;
 
-//lbs += 2.5; //GMX25 + long32#@27 + 22@14 string
+    //lbs += 2.5; //GMX25 + long32#@27 + 22@14 string
 
-spine = -100 * length + 4000 - 100 * (lbs - 18.5) / 4;
-console.log(spine)
+    spine = -100 * length + 4000 - 100 * (lbs - 18.5) / 4;
+    document.getElementsByClassName('spine')[0].innerHTML = 'Spine: ' + spine
+    console.log(spine)
+}
