b524efd7db9efccda015a67b8b1d4ce715babb6f
lbarjak <barjak.laszlo@gmail.com>
2019-02-15 14:24:52 +0100

    out of range

--- arrows.js
+++ arrows.js
@@ -4,12 +4,17 @@ function arrows() {
     lbs = Number(document.getElementById("lbs").value)
     length = Number(document.getElementById("inches").value)
     point = Number(document.getElementById("point").value)
-    
+
     lbs = lbs + (point - 100) * 0.15;
 
     //lbs += 2.5; //GMX25 + long32#@27 + 22@14 string
 
-    spine = -100 * length + 4000 - 100 * (lbs - 18.5) / 4;
-    document.getElementsByClassName('spine')[0].innerHTML = 'spine: ' + spine
+    spine = -100 * length + 4000 - 100 * (lbs - 18.5) / 4
+    if(spine >= 280) {
+        document.getElementsByClassName('spine')[0].innerHTML = 'spine: ' + spine.toFixed(0)
+    } else {
+        document.getElementsByClassName('spine')[0].innerHTML = 'spine: out of range'
+    }
+    
     console.log(spine)
 }
