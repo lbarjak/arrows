0d0fb26be3098e0a3a5bc5d0c2a6e55e271e9016
ehorvath <ehorvath@G770.barjak.eu>
2019-10-28 20:50:03 +0100

    1.0 refactored

--- arrows.js
+++ arrows.js
@@ -5,23 +5,17 @@ function arrows() {
     length = Number(document.getElementById("inches").value)
     point = Number(document.getElementById("point").value)
 
-    if (point < 50 || point > 150 || lbs < 17 || lbs > 69 || length < 21 || length > 33){
-        message();
-    } else {
+    if (point >= 50 && point <= 150 && lbs >= 17 && lbs <= 69 && length >= 21 && length <= 33){
         lbs = lbs + (point - 100) * 0.15;
         spine = -100 * length + 4000 - 100 * (lbs - 18.5) / 4;
-        console.log("1. " + spine);
+        console.log(spine);
     }
     
-    if (spine < 340 || spine > 1800) {
-        message();
-    } else {
-        console.log("2. " + spine);
+    if (spine >= 340 && spine <= 1800) {
         document.getElementsByClassName('spine')[0].innerHTML = spine.toFixed(0);
         document.getElementsByClassName('message')[0].innerHTML = "";
+    } else {
+        document.getElementsByClassName('message')[0].innerHTML = "Out of range!";
+        document.getElementsByClassName('spine')[0].innerHTML = "";
     }
-}
-function message() {
-    document.getElementsByClassName('message')[0].innerHTML = "Out of range!";
-    document.getElementsByClassName('spine')[0].innerHTML = "";
 }
