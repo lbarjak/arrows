6590e7f64479c29adff818c63826ec2fef25fd59
lbarjak <barjak.laszlo@gmail.com>
2019-10-29 00:05:34 +0100

    new spine calculation

--- arrows.js
+++ arrows.js
@@ -1,17 +1,17 @@
 function arrows() {
     var lbs = 0, length = 0, point = 0, spine = 0
-    
+
     lbs = Number(document.getElementById("lbs").value)
     length = Number(document.getElementById("inches").value)
     point = Number(document.getElementById("point").value)
 
-    if (point >= 50 && point <= 150 && lbs >= 17 && lbs <= 69 && length >= 21 && length <= 33){
+    if (point >= 50 && point <= 150 && lbs >= 17 && lbs <= 69 && length >= 21 && length <= 33) {
         lbs = lbs + (point - 100) * 0.15;
-        spine = -100 * length + 4000 - 100 * (lbs - 18.5) / 4;
+        spine = -25 * lbs + 2362.5 - (length - 21) * 100;
         console.log(spine);
     }
-    
-    if (spine >= 340 && spine <= 1800) {
+
+    if (spine >= 300 && spine <= 2000) {
         document.getElementsByClassName('spine')[0].innerHTML = spine.toFixed(0);
         document.getElementsByClassName('message')[0].innerHTML = "";
     } else {
